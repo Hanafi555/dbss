761e0d2f2b5ccc580b1d501854a8b835407dafad
Hanafi <hanafi92@hotmail.com>
2025-07-21 17:43:17 +0000

    updates

--- app.py
+++ app.py
@@ -5,14 +5,16 @@ import requests
 
 import os
 
-# ------------------- Load Models Once at Startup -------------------
-# These files must be in the same folder as app.py!
-cv = joblib.load("cv_encoder.pkl")
-model = joblib.load("model.pkl")
+
 os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 # for cloud ..........
+
+# ------------------- Load Models Once at Startup -------------------
+# These files must be in the same folder as app.py!
+cv = joblib.load("cv_encoder.pkl")
+model = joblib.load("model.pkl")
 
 app = Flask(__name__)
 
