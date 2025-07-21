f59e8ddc07cddedb998cbba11b734e64c9bee22f
Hanafi <hanafi92@hotmail.com>
2025-07-21 18:12:47 +0000

    Update this instead

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ import requests
 
 import os
 
-
-os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
+# ------------------- Load Models Once at Startup -------------------
+# These files must be in the same folder as app.py!
+cv = joblib.load("cv_encoder.pkl")
+model = joblib.load("model.pkl")
+#os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 # for cloud ..........
